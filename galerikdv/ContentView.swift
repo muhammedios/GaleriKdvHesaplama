3a55bcf6cf37f3d85b7c33057494cc605b9b85c5
muhammedios <yilmaz34muhammed@icloud.com>
2026-04-12 10:55:06 +0300

    Initial Commit

---
+++ galerikdv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  galerikdv
+//
+//  Created by Muhammed Yılmaz on 12.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ galerikdv/galerikdvApp.swift
@@ -0,0 +1,17 @@
+//
+//  galerikdvApp.swift
+//  galerikdv
+//
+//  Created by Muhammed Yılmaz on 12.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct galerikdvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
